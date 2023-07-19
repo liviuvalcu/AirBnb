30fb1b45bc694e1fc70af616d7bb5d0f3cae1c08
Liviu Valcu <liviu.valcu@yahoo.com>
2023-07-19 16:02:59 +0300

    jwt

---
+++ src/main/java/com/reply/airbnbdemo/Accesslog.java
@@ -0,0 +1,29 @@
+package com.reply.airbnbdemo;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "accesslog")
+public class Accesslog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "methodName", nullable = false)
+    private String methodName;
+
+}
