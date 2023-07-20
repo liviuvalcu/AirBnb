514094247ee74980c49f3bfdb7c01b14b3ff7418
Liviu Valcu <liviu.valcu@yahoo.com>
2023-07-20 11:26:17 +0300

    aop

---
+++ src/main/java/com/reply/airbnbdemo/dao/RegistrationDao.java
@@ -0,0 +1,12 @@
+package com.reply.airbnbdemo.dao;
+
+import com.reply.airbnbdemo.bean.PropertyBean;
+import com.reply.airbnbdemo.bean.UserBean;
+import com.reply.airbnbdemo.enums.UserType;
+
+public class RegistrationDao {
+    private SignUpRequest signUpRequest;
+    private UserBean userBean;
+    private UserType userType;
+    private PropertyBean propertyBean;
+}
